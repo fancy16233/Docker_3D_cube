fe0ec34f5eeddd920741faf8ac05ee34f100f1ec
Unknown <fancy16233@Gmail.com>
2017-06-23 11:14:31 +0800

    update docker.sh and adding database file

--- docker.sh
+++ docker.sh
@@ -6,4 +6,7 @@ sudo docker rmi app
 
 sudo docker build -t app .
 sudo docker start afaf7b6bf309 
-sudo docker run  -i -t -d --name=app -p 8080:80 app 
+sudo docker run  -i -t -d --name=app -p 8080:80 app
+
+sudo docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=mypass' -v /home/fancy16233/Desktop/SQLfile:/var/opt/mssql -p  1433:1433 -d microsoft/mssql-server-linux
+ 
